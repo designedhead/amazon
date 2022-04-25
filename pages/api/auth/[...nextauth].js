d5c63abe1a8746c43cec2e42eed760d5750b7027
designedhead <rdesignedhead@gmail.com>
2022-04-25 16:44:49 +0100

    deploy

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -10,4 +10,5 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-})
+  secret: process.env.NEXTAUTH_SECRET,
+});
